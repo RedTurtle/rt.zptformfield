47c7ef5819e50ea9069d86642caf2da1e68d7586
Luca Fabbri <luca@keul.it>
2014-10-09 15:16:00 +0200

    HISTORY to rst format

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='rt.zptformfield',
       version=version,
       description="Some ZPT macros for Zope/Plone HTML forms development",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
